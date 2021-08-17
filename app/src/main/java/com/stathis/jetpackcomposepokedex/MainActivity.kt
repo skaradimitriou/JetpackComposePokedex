edb6925924b370246014a3034620ec84616febac
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-08-17 23:55:16 +0300

    UI | 1

--- app/src/main/java/com/stathis/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/stathis/jetpackcomposepokedex/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.stathis.jetpackcomposepokedex.pokemonList.PokemonListScreen
 import com.stathis.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen"
                 ){
                     composable("pokemon_list_screen"){
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable(
                         "pokemon_detail_screen/{dominantColor}/{pokemonName}",
